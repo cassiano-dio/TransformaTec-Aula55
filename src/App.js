85ef04b511ab6491c579316cc8822ca92fa7241b
cassiano-dio <cassiano@dio.me>
2022-05-25 15:58:40 -0300

    Novo commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import Car from './components/Car'
+import PersonRegister from './components/PersonRegister'
+import GameScore from './components/GameScore';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloWorld name="Cassiano" />
+      <Car owner="Cassiano Pé de Chumbo q leva multa toda hora"/>
+      <PersonRegister name="Cassiano" age={32} lastName="Peres" job="Dev" />
+      <GameScore points={0}/>
     </div>
   );
 }
